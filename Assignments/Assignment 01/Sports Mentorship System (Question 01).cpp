0c5389ac31429244aa3add409fea7a94f3c8c890
Mutahir Ahmed Khan <178812198+Mutahir-18@users.noreply.github.com>
2025-02-16 11:56:25 +0500

    Update Sports Mentorship System (Question 01).cpp

--- Assignments/Assignment 01/Sports Mentorship System (Question 01).cpp
+++ Assignments/Assignment 01/Sports Mentorship System (Question 01).cpp
@@ -1,13 +1,11 @@
 #include <iostream>
 using namespace std;
 
-//mentor Assigned's data-type would be mentor and wil store whole object of mentor (miss abeeha)
-
-//"-----Forward declaration----------"//
+//-------Forward declaration----------//
 class Skill;
 class Mentor;
 class Sports;
-//"-----------------------------------"//
+//-----------------------------------//
 
 class Student{
     public:
@@ -15,16 +13,22 @@ class Student{
     string studentName;
     int age;
     string* sportsInterests;
-    string mentorAssigned;
+    Mentor* mentorAssigned;
 
     //Constructor 
-    Student(int id, string name,int a) : studentID(id), studentName(name), age(a) {}
+    Student(int id, string name,int a) : studentID(id), studentName(name), age(a) {
+        sportsInterests = new string[5];
+    }
+    //Destructor 
+    ~Student(){
+        delete[] sportsInterests;
+    }
 
     //Mentor Registration
-    void registerForMentorship(Mentor m);
+    void registerForMentorship(Mentor* m);
 
     //Sports Interest Update
-    void updateSportsInterest(Sports s);
+    void updateSportsInterest(Sports* s);
 
     //Mentor's Detail
     void viewMentorDetails();
@@ -56,16 +60,23 @@ class Sports{
     int sportID;
     string sportName; 
     string description;
-    string* requiredSkill;
+    Skill** requiredSkill;
+    int skillCount = 0;
 
     //Constructor
-    Sports(int id, string name, string desc) : sportID(id), sportName(name), description(desc) {}
+    Sports(int id, string name, string desc) : sportID(id), sportName(name), description(desc) {
+        requiredSkill = new Skill*[5];
+    }
+    //Destructor 
+    ~Sports(){
+        delete[] requiredSkill;
+    }
 
     //add Skill
-    void addSkill(Skill s);
+    void addSkill(Skill* s);
 
     // //Remove Skills
-    void removeSkill(Skill s);
+    void removeSkill(Skill* s);
 };
 
 class Skill{
@@ -85,7 +96,7 @@ class Skill{
 };
 
 //--------------------Defining Functions------------------------//
-//SKILL SECTION
+//SKILL SECTION (DONE) ---------------------------------------//
 void Skill :: showSkillDetail(){
     cout << "--------------------" << endl;
     cout << "Skill Name: " << skillName << endl;
@@ -96,10 +107,61 @@ void Skill :: showSkillDetail(){
 void Skill :: updateSkillDescription(string newDescription){
     description = newDescription;
 }
+//-------------------------------------------------------------//
 
-//SPORTS SECTION
-//STUDENT SECTION 
-//MENTOR SECTION 
+//SPORTS SECTION------------------------------------------------
+void Sports :: addSkill(Skill* s){
+    if (skillCount < 5){
+        requiredSkill[skillCount++] = s;
+        cout << "Skill " << s->skillName<< " Added Successfully " << "To " << sportName <<  endl;
+    }
+    else{
+        cout << "Maximum Skill have been added" << endl;
+    }
+}
+
+void Sports :: removeSkill(Skill* s){
+    for (int i = 0; i < skillCount; ++i) {
+        if (requiredSkill[i] == s) {
+            for (int j = i; j < skillCount - 1; ++j) {
+                requiredSkill[j] = requiredSkill[j + 1]; 
+            }
+            skillCount--;
+            cout << "Skill " << s->skillName << " removed from " << sportName << endl;
+            return;
+        }
+    }
+    cout << "Skill " << s->skillName << " not found in " << sportName << endl;
+}
+//---------------------------------------------------------------------
+
+//STUDENT SECTION------------------------------------------------------//
+void Student :: registerForMentorship(Mentor* m){
+    mentorAssigned = m;
+}
+
+void Student :: updateSportsInterest(Sports* s){
+    static int SportInterestCount = 0;
+    if(SportInterestCount < 5){
+        cout << "Enter the Sports you wanted to add: ";
+        cin >> sportsInterests[SportInterestCount];
+        SportInterestCount++;
+    }
+    else{
+        cout << "Already Maximum Sport Interest" << endl;
+    }
+}
+
+void Student :: viewMentorDetails(){
+    if(mentorAssigned){
+        cout << "Mentor Name: " << mentorAssigned->name << endl;
+        cout << "Mentor ID: " << mentorAssigned->mentorID << endl;
+    }
+    else{
+        cout << "No Mentor Assigned" << endl;
+    }
+}
+//MENTOR SECTION -----------------------------------------------------//
 //--------------------------------------------------------------//
 int main(){
 
