291475d08c193dd3cff56441920229b9f937ac6c
Mutahir Ahmed Khan <178812198+Mutahir-Ahmed-Khan@users.noreply.github.com>
2025-04-03 20:23:03 +0500

    Update Lab Management System (Question 04).cpp

--- Assignments/Assignment 02/Lab Management System (Question 04).cpp
+++ Assignments/Assignment 02/Lab Management System (Question 04).cpp
@@ -3,7 +3,7 @@
 #include <string>
 using namespace std;
 
-//Global Function of Hash
+// Global Function of Hash
 string GenHash(string pass) {
     int hash = 5381;
     string hashed = "";  
@@ -13,12 +13,10 @@ string GenHash(string pass) {
         hash = hash * 33 + pass[i]; 
         hashed += char(hash % 128);  
     }
-
     return hashed;
 }
-
 class User{
-    private:
+private:
     string name;
     int ID;
     string email;
@@ -26,40 +24,65 @@ class User{
     string Pass;
     string finalHashPass;
     int size;
+public:
+    // Constructor
+    User(string n, int id, string e, int tempSize, string p) 
+        : name(n), ID(id), email(e), Pass(p) {
+        size = tempSize;
+        permissions = new string[size];
+        finalHashPass = GenHash(p);  
 
-    public:
-    //Constructor
-    User(string n, int id, string e,int tempSize,string p) 
-        : name(n), ID(id), email(e),Pass(p){
-            size = tempSize;
-            permissions = new string[size];
-            finalHashPass = GenHash(p);
-            for(int i = 0; i < size; i++){
-                permissions[i] = "---";
-            }
-            cout << right << setw(30) << "*---(User)---*" << endl;
+        for(int i = 0; i < size; i++) {
+            permissions[i] = "---";
         }
-    //Destructor
-        ~User() {
-            delete[] permissions;
-        }
-    //Display Function
-    void display(){
+        cout << right << setw(30) << "*---(User Created)---*" << endl;
+    }
+    // Destructor
+    ~User() {
+        delete[] permissions;
+    }
+    // Display Function
+    void display() {
         cout << right << setw(30) << "*********************************************" << endl;
         cout << right << setw(19) << "NAME: " << name << endl;
-        cout << right << setw(20) << "EMAIL: " << email <<endl;
-        cout << right << setw(17) << "ID: " << ID <<endl;
-        cout << right << setw(23) << "PASSWORD: " << finalHashPass <<endl;
-        for(int i = 0; i < size; i++){
+        cout << right << setw(20) << "EMAIL: " << email << endl;
+        cout << right << setw(17) << "ID: " << ID << endl;
+        cout << right << setw(30) << "PASSWORD (Hash): " << finalHashPass << endl;
+        for (int i = 0; i < size; i++) {
             cout << right << setw(25) << "Permission #" << i + 1 << ": " << permissions[i] << endl;
         }
-        cout << right << setw(30) << "*********************************************" << endl;  
+        cout << right << setw(30) << "*********************************************" << endl;
+    }
+    // Authentication Function
+    void authenticate() {
+        string enterPass;
+        cout << "Enter the Password: ";
+        getline(cin, enterPass);  
+
+        if (GenHash(enterPass) == finalHashPass) {
+            cout << "Verified" << endl;
+        } else {
+            cout << "Not Verified" << endl;
+        }
+    }
+    //Getter and Setter 
+    string* getPermission(){
+        return permissions;
     }
 };
 
+class Student : public User{
+    public:
+    //Construtor 
+    Student(string n, int id, string e, string p) : User(n,id,e,1,p) {
+        getPermission()[0] = "Only Allowed to submit Assignments";
+        cout << right << setw(28) << "----- Student -----" << endl;
+    }
 
-int main(){
-    User user01("Mutahir",1020,"MutahirHere18@gmail.com",3,"MAK000");
-    user01.display();
+};
+
+int main() {
+    Student s1("Mutahir", 1020, "MutahirHere18@gmail.com","MAK000");
+    s1.display();    
     return 0;
 }
